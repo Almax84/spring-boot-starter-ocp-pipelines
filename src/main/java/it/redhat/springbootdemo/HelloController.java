377d1a20fa88c84dd4ed999bb65225ef9ee9980b
Davide Scrimieri <dscrimie@redhat.com>
2021-11-03 18:45:25 +0100

    turn tu web!

--- src/main/java/it/redhat/springbootdemo/HelloController.java
+++ src/main/java/it/redhat/springbootdemo/HelloController.java
@@ -4,28 +4,29 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+@Controller
 public class HelloController {
 
     @Autowired
     private Environment env;
 
-    @GetMapping("/")
-    public String index() {
+    @GetMapping("/hello")
+    public String index(Model model) {
 
 	String startingMessage = "MY VERY FIRST DEPLOY! \n";
         String hostname = env.getProperty("HOSTNAME");
         String appMsg = env.getProperty("APP_MSG");
         if(!StringUtils.isEmpty(hostname) && !StringUtils.isEmpty(appMsg)){
-            return startingMessage + "Greeting from "+hostname+"!!"+" APP_MSG read from environment is: "+appMsg;
+            model.addAttribute("message2","APP_MSG from config-map is: "+appMsg);
         }
-        else if(!StringUtils.isEmpty(hostname)){
-            return startingMessage + "Greeting from "+hostname+"!!";
-        }
+//        else if(!StringUtils.isEmpty(hostname)){
+//            return startingMessage + "Greeting from "+hostname+"!!";
+//        }
 
-        return startingMessage + "Greetings from Spring Boot!";
+        model.addAttribute("message",startingMessage);
+        return "hello";
     }
 
 }
